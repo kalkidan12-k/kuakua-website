cbf8c71508128d9852c812e737df7bb1f45258cd
kalkidan12-k <kkflemariam@gmail.com>
2025-03-19 14:28:51 -0700

    k

--- my-react-app/src/componet/Navbar/Part1.js
+++ my-react-app/src/componet/Navbar/Part1.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Part1() {
   return (
-    <div className='relative '>
+    <div className='relative'>
       <div className='container z-[-1]'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px] '>
-{/* text section*/}
-<h1 className='font-'>FREASH AND HEALTY SNACK DELIVERY IN KUAKUA</h1>
-<p>
-    Delicious snak delivered to your door from kuakua.
-</p>
-{/* image  section*/}
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]'>
+          {/* Text Section */}
+          <div>
+            <h1 className='font-bold text-2xl'>FRESH AND HEALTHY SNACK DELIVERY IN KUAKUA</h1>
+            <p className='mt-4 text-lg'>
+              Delicious snacks delivered to your door from Kuakua.
+            </p>
+          </div>
+          {/* Image Section  <div>
+            <img src={te} alt='Fast Food' className='w-full h-auto rounded-lg shadow-lg' />
+          </div>*/}
+          
         </div>
       </div>
     </div>
-  )
+  );
 }
