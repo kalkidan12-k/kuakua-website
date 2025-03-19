b09b19f57711657c69c3071f8b69017e08deed7d
kalkidan12-k <kkflemariam@gmail.com>
2025-03-18 22:05:07 -0700

    k

--- my-react-app/src/componet/Navbar/Part1.js
+++ my-react-app/src/componet/Navbar/Part1.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Part1() {
   return (
-    <div>
-      
+    <div className='relative '>
+      <div className='container z-[-1]'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]'>
+{/* text section*/}
+
+{/* image  section*/}
+        </div>
+      </div>
     </div>
   )
 }
