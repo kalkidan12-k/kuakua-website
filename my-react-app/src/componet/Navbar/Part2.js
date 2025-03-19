281029bf7a887f65cac7816d4e7a111b81d73a6d
kalkidan12-k <kkflemariam@gmail.com>
2025-03-19 16:47:47 -0700

    q

--- my-react-app/src/componet/Navbar/Part2.js
+++ my-react-app/src/componet/Navbar/Part2.js
@@ -9,14 +9,26 @@ export default function Part2() {
 tast the HEALTHY difference
       </h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 py-10'>
-        
-      </div>
-      <div>
+       <div>
         <p>
             {""}
-            
+            we know that <span>time</span> is the  greatest value modern world 
+            our healthy snack plan delivery services good tast in kuakua
+            who want eating healthy and tasty food
         </p>
+      </div> 
       </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 px-20'>
+       <div>
+        <p>
+            {""}
+            we know that <span>time</span> is the  greatest value modern world 
+            our healthy snack plan delivery services good tast in kuakua
+            who want eating healthy and tasty food
+        </p>
+      </div> 
+      </div>
+      
       </div>
     </div>
   )
