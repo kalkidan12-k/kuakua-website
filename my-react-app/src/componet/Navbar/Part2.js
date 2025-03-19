069a62c8caa9c94af6dc40f4ec1714eb35cca340
kalkidan12-k <kkflemariam@gmail.com>
2025-03-19 16:34:23 -0700

    abouthealth

--- my-react-app/src/componet/Navbar/Part2.js
+++ my-react-app/src/componet/Navbar/Part2.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import PrimaryButton from '../../Shared/PrimaryButton'
 
 export default function Part2() {
   return (
-    <div>
-      
+    <div className='container py-15 relative'>
+      <div>
+      <h1 className='uppercase py-8 tracking-wider text-2xl font-semibold text-dark text-center'>
+tast the HEALTHY difference
+      </h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 py-10'>
+        
+      </div>
+      <div>
+        <p>
+            {""}
+            
+        </p>
+      </div>
+      </div>
     </div>
   )
 }
