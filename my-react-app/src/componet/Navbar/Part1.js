89d886f2f01f29f7114bc3184598278caaf1a388
kalkidan12-k <kkflemariam@gmail.com>
2025-03-18 22:16:14 -0700

    l

--- my-react-app/src/componet/Navbar/Part1.js
+++ my-react-app/src/componet/Navbar/Part1.js
@@ -4,9 +4,12 @@ export default function Part1() {
   return (
     <div className='relative '>
       <div className='container z-[-1]'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px] '>
 {/* text section*/}
-
+<h1 className='font-'>FREASH AND HEALTY SNACK DELIVERY IN KUAKUA</h1>
+<p>
+    Delicious snak delivered to your door from kuakua.
+</p>
 {/* image  section*/}
         </div>
       </div>
