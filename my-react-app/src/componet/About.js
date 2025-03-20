804548e7b21efa6a3ea3fb1ce484d3272bdefe6b
kalkidan12-k <kkflemariam@gmail.com>
2025-03-20 12:17:12 -0700

    s

--- my-react-app/src/componet/About.js
+++ my-react-app/src/componet/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import bg from '../images/bg.jpg'
 
 export default function About() {
   return (
-    <div>
-     <div>
+    <div >
+     <div style={bg}>
         <h1 className='uppercase text-center text-2xl font-semibold text-dark tracking-wider pt-20'>
             About Us
         </h1>
