4363b4673cdd08899cbd97d981ea558850c0aecc
kalkidan12-k <kkflemariam@gmail.com>
2025-03-20 16:40:36 -0700

    o

--- my-react-app/src/componet/Navbar/Popup.js
+++ my-react-app/src/componet/Navbar/Popup.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+    import { IoCloseOutline } from 'react-icons/io5'
+
 export default function Popup( {showPopup,setshowPopup}) {
   return (
     <>
       {!showPopup && (<div> <div className='h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm'>
-        <div className='fixed top-1/2 left-1/2 shadow:md -translate-x-1/2 -translate-y-1/2 bg-white p-10 rounded-md duration-200 w-[300px]'>
+        <div className='fixed top-1/2 left-1/2 shadow:md -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md duration-200 w-[300px]'>
+        <div className='flex items-center justify-between'>
+            <h1 className='text-2xl font-bold text-dark'>
+                login
+            </h1>
+        </div>
+        <div>
+            <IoCloseOutline/>
+        </div>
 
         </div>
       </div>
