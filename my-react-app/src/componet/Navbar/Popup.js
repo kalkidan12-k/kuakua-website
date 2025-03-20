9fccb09a5402fe6f5ecbafa2800b0ca2634403b4
kalkidan12-k <kkflemariam@gmail.com>
2025-03-20 16:23:15 -0700

    pop

--- my-react-app/src/componet/Navbar/Popup.js
+++ my-react-app/src/componet/Navbar/Popup.js
@@ -4,7 +4,9 @@ export default function Popup( {showPopup,setshowPopup}) {
   return (
     <>
       {!showPopup && (<div> <div className='h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm'>
-        hello
+        <div className='fixed top-1/2 left-1/2 shadow:md -translate-x-1/2 -translate-y-1/2 bg-white p-10 rounded-md duration-200 w-[300px]'>
+
+        </div>
       </div>
         </div>)}
     </>
